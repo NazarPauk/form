7256a5850790af2f8a869ef25b201e920994ec70
NazarPauk <nazarpauk5@gmail.com>
2025-10-02 14:41:26 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -837,7 +837,7 @@ function buildVCard(meta) {
   const tel = COMPANY_PHONE;
   const site = 'https://dolota.ua';
   const email = 'info@dolota.ua';
-  const chatbot = 'https://t.me/test421_bot';
+  const chatbot = 'https://t.me/dolota_pr_bot';
   const note = 'ЗБЕРЕЖІТЬ НОВИЙ КОНТАКТ ▼▼▼ Бурові машини, компресори, бурове обладнання та інструмент. Чатбот: ' + chatbot;
   const fn = `${family} ${given} ${additional}`.trim();
   return [
